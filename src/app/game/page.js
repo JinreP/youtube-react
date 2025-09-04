72a84f29d054e0cc001a76343c94f44fa8c091b2
JinreP <subeegod@gmail.com>
2025-09-04 12:47:33 +0800

    balloon loop

--- src/app/game/page.js
+++ src/app/game/page.js
@@ -1,14 +1,11 @@
 import { Balloon } from "../../components/Game";
-
+const colorTheme = ["red", "blue", "purple", "violet", "black", "orange", "yellow", "green", "pink"];
 export default function Home() {
   return (
-    <div className="flex gap-5 w-full h-screen  justify-center items-center">
-      {" "}
-      <Balloon backgroundColor="bg-red-400"  />
-      <Balloon />
-      <Balloon />
-      <Balloon />
-      <Balloon />
+    <div className="flex gap-5 w-screen h-screen justify-center items-center">
+      {colorTheme.map((color) => (
+        <Balloon key={color} color={color} />
+      ))}
     </div>
   );
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,9 +1,9 @@
 export function Balloon(props) {
   return (
     <div className="flex flex-col animate-bounce items-center justify-center">
-      <div className="border-2  rounded-[50px] bg-pink-400 w-[100px]  h-[100px]" />
+      <div className="border-0 rounded-[45px] bg-pink-400 w-[100px]  h-[100px]" style={{ backgroundColor: props.color }} />
 
-      <div className="w-[3px]  h-[120px] bg-black border-solid" />
+      <div className="w-[3px]  h-[100px] bg-black border-solid" />
     </div>
   );
 }
