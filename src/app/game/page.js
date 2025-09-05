10e7a0f714c535592237a82f463a4dde7fa09c61
Subeedei <subeegod@gmail.com>
2025-09-05 12:27:12 +0800

    balloon appear randomly

--- src/app/game/page.js
+++ src/app/game/page.js
@@ -1,11 +1,37 @@
 import { Balloon } from "../../components/Game";
-const colorTheme = ["red", "blue", "purple", "violet", "black", "orange", "yellow", "green", "pink"];
+const colorTheme = [
+  "red",
+  "blue",
+  "purple",
+  "violet",
+  "black",
+  "orange",
+  "yellow",
+  "green",
+  "pink",
+];
+
+function getRandomPosition() {
+  const top = Math.floor(Math.random() * 80);
+  const left = Math.floor(Math.random() * 80);
+  return { top: `${top}%`, left: `${left}%` };
+}
+
 export default function Home() {
   return (
-    <div className="flex gap-5 w-screen h-screen justify-center items-center">
-      {colorTheme.map((color) => (
-        <Balloon key={color} color={color} />
-      ))}
+    <div className="relative w-screen h-screen">
+      {colorTheme.map((color, index) => {
+        const position = getRandomPosition();
+        return (
+          <Balloon
+            num={index + 1}
+            key={index}
+            color={color}
+            top={position.top}
+            left={position.left}
+          />
+        );
+      })}
     </div>
   );
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,7 +1,14 @@
 export function Balloon(props) {
   return (
-    <div className="flex flex-col animate-bounce items-center justify-center">
-      <div className="border-0 rounded-[45px] bg-pink-400 w-[100px]  h-[100px]" style={{ backgroundColor: props.color }} />
+    <div
+      className="flex flex-col absolute animate-bounce items-center justify-center"
+      style={{ top: props.top, left: props.left }}
+    >
+      <div
+        className="border-0 relative rounded-[45px] bg-pink-400 w-[100px]  h-[100px]"
+        style={{ backgroundColor: props.color }}
+      />
+      <p className="absolute  bottom-40">{props.num}</p>
 
       <div className="w-[3px]  h-[100px] bg-black border-solid" />
     </div>
