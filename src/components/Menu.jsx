c0a4a30c8a3e163512d30f3c60bc9f027da508f5
JinreP <subeegod@gmail.com>
2025-09-06 13:08:42 +0800

    menu fixed

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,60 +1,60 @@
 export function Menu() {
   return (
-    <div className="flex gap-5 cursor-pointer justify-center pt-10 overflow-hidden  ps-40 ">
-      <p className="rounded-2xl text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[50px] h-[30px] bg-gray-500 ">
+    <div className="flex gap-5  cursor-pointer justify-center pt-10 overflow-scroll  ps-170 ">
+      <p className="rounded-2xl p-4 text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[70px] h-[30px] bg-gray-500 ">
         All
       </p>
-      <p className="rounded-2xl text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[50px] h-[30px] bg-gray-500  ">
+      <p className="rounded-2xl p-4 text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[70px] h-[30px] bg-gray-500  ">
         Music
       </p>
-      <p className="rounded-2xl text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[50px] h-[30px] bg-gray-500 ">
+      <p className="rounded-2xl p-4 text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[70px] h-[30px] bg-gray-500 ">
         Mixes
       </p>
-      <p className="rounded-2xl text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[70px] h-[30px] bg-gray-500 ">
+      <p className="rounded-2xl p-4 text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[80px] h-[30px] bg-gray-500 ">
         Gaming
       </p>
-      <p className="rounded-2xl text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[140px] h-[30px] bg-gray-500 ">
+      <p className="rounded-2xl p-4 text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[140px] h-[30px] bg-gray-500 ">
         Counter-Strike 2
       </p>
-      <p className="rounded-2xl text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[90px] h-[30px] bg-gray-500 ">
+      <p className="rounded-2xl p-4 text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[90px] h-[30px] bg-gray-500 ">
         Hip Hop
       </p>
-      <p className="rounded-2xl text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[130px] h-[30px] bg-gray-500 ">
+      <p className="rounded-2xl p-4 text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[130px] h-[30px] bg-gray-500 ">
         Comic books
       </p>
-      <p className="rounded-2xl text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[100px] h-[30px] bg-gray-500 ">
+      <p className="rounded-2xl p-4 text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[100px] h-[30px] bg-gray-500 ">
         Electropop
       </p>
-      <p className="rounded-2xl text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[50px] h-[30px] bg-gray-500 ">
+      <p className="rounded-2xl p-4 text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[70px] h-[30px] bg-gray-500 ">
         J-Pop
       </p>
-      <p className="rounded-2xl text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[180px] h-[30px] bg-gray-500 ">
+      <p className="rounded-2xl p-4 text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[180px] h-[30px] bg-gray-500 ">
         Contemporary R&B
       </p>
-      <p className="rounded-2xl text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[90px] h-[30px] bg-gray-500 ">
+      <p className="rounded-2xl p-4 text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[90px] h-[30px] bg-gray-500 ">
         Thrillers
       </p>
-      <p className="rounded-2xl text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[100px] h-[30px] bg-gray-500 ">
+      <p className="rounded-2xl p-4 text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[100px] h-[30px] bg-gray-500 ">
         Free Music
       </p>
-      <p className="rounded-2xl text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[90px] h-[30px] bg-gray-500 ">
+      <p className="rounded-2xl p-4 text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[90px] h-[30px] bg-gray-500 ">
         Text
       </p>
-      <p className="rounded-2xl text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[100px] h-[30px] bg-gray-500 ">
+      <p className="rounded-2xl p-4 text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[100px] h-[30px] bg-gray-500 ">
         Indie pop music Music
       </p>
 
-      <p className="rounded-2xl text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[90px] h-[30px] bg-gray-500 ">
+      <p className="rounded-2xl p-4 text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[90px] h-[30px] bg-gray-500 ">
         CS2
       </p>
-      <p className="rounded-2xl text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[100px] h-[30px] bg-gray-500 ">
+      <p className="rounded-2xl p-4 text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[100px] h-[30px] bg-gray-500 ">
         Dota 2
       </p>
 
-      <p className="rounded-2xl text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[90px] h-[30px] bg-gray-500 ">
+      <p className="rounded-2xl p-4 text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[90px] h-[30px] bg-gray-500 ">
         MONGOLZ
       </p>
-      <p className="rounded-2xl text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[100px] h-[30px] bg-gray-500 ">
+      <p className="rounded-2xl p-4 text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[100px] h-[30px] bg-gray-500 ">
         Vlogs
       </p>
     </div>
