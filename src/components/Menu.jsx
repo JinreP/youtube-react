ccf03fae5504aa435fd5351c2d35725c2634c131
Subeedei <subeegod@gmail.com>
2026-01-05 21:49:28 +0800

    sda

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,7 +2,7 @@ export function Menu() {
   return (
     <div className="fixed top-16 left-0 right-0 flex gap-5 cursor-pointer justify-start py-3 overflow-scroll bg-black z-30 pl-60">
       <p className="rounded-2xl p-4 text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[70px] h-[30px] bg-gray-500 ">
-        All
+        AAA
       </p>
       <p className="rounded-2xl p-4 text-center hover:bg-black hover:text-white text-1xl pt-0.5 w-[70px] h-[30px] bg-gray-500  ">
         Music
